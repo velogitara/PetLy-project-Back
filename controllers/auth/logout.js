ccd2be9198caddeb0fc0ce29e865918c20f0c8b2
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-11-10 12:35:52 +0200

    fix status on logout, add server.log to gitignire

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -5,7 +5,7 @@ const logOut = async (req, res) => {
   await User.findByIdAndUpdate(_id, { token: null });
   res.json({
     status: 'success',
-    code: 200,
+    code: 204,
     data: {
       message: 'logout success ',
     },
