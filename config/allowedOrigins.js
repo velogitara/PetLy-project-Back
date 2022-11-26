eb03dc00d1927ad1ac18451ae0f7ddc988be6267
unknown <velogitara@gmail.com>
2022-11-26 23:39:39 +0200

    fix origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const { PORT } = process.env;
 const allowedOrigins = [
   'http://localhost:3000',
   `https://petly-project-goit.herokuapp.com:${PORT}`,
-  `https://velogitara.github.io:${PORT}`,
+  'https://velogitara.github.io',
 ];
 
 module.exports = allowedOrigins;
