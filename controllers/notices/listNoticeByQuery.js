eca010fa0ed9b98e23c2b776390638f47ef5495f
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-11-27 18:07:31 +0200

    add total

--- controllers/notices/listNoticeByQuery.js
+++ controllers/notices/listNoticeByQuery.js
@@ -3,8 +3,6 @@ const { requestError } = require('../../helpers');
 
 const listNoticeByQuery = async (req, res) => {
   const { page = 1, limit = 8, query, category } = req.query;
-  // console.log(req.params);
-  console.log(req.query);
   const skip = (page - 1) * limit;
 
   const result = await Notice.find(
