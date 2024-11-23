01efbc72c9e005850195557078476b3227b6dc19
Oleksandr Pavlishchev <velogitara@gmail.com>
2024-11-23 18:38:58 +0400

    change allowedCors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,13 @@
-const { PORT } = process.env;
+// const { PORT } = process.env;
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   // `https://petly-project-goit.herokuapp.com:${PORT}`,
+//   `https://pet-ly-project-back.vercel.app:${PORT}`,
+//   'https://velogitara.github.io',
+// ];
 const allowedOrigins = [
   'http://localhost:3000',
-  // `https://petly-project-goit.herokuapp.com:${PORT}`,
-  `https://pet-ly-project-back.vercel.app:${PORT}`,
+  'https://pet-ly-project-back.vercel.app', // No need for the dynamic port
   'https://velogitara.github.io',
 ];
 
